f9f2969fd879d9925035af1af95c9e828cca5643
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-19 18:00:15 -0600

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,32 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
 
-def summon_captain_planet():
-    pass
+    i = 0
+    for dwarf in dwarves:
+        i += 1
+        print(f"{i}. {dwarf}")
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def summon_captain_planet(elements):
+
+    formatted_elements = [element.capitalize() + "!" for element in elements]
+    print(formatted_elements)
+    return formatted_elements
+
+summon_captain_planet(["teutons", "japanese", "aztecs"])
+
+def long_planeteer_calls(elements):
+    long_found = False
+    for element in elements:
+        if len(element) > 4:
+            long_found = True
+    return long_found
+
+def find_the_cheese(snacks):
+    snack_found = False
+    for snack in snacks:
+        if snack == "cheddar" or snack == "gouda" or snack == "camembert":
+            snack_found = True
+            return snack
+    if snack_found == False:
+        return None
+        
